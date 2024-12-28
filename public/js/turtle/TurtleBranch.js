b83d4f99c085a23be08b7da3de76afdc15d28895
David Tudury <david.tudury@gmail.com>
2024-12-28 15:09:30 -0800

    streaming for turtle branches

--- public/js/turtle/TurtleBranch.js
+++ public/js/turtle/TurtleBranch.js
@@ -1,10 +1,13 @@
 import { Recaller } from '../utils/Recaller.js'
 import { squashTurtle, U8aTurtle } from './U8aTurtle.js'
+import { combineUint8ArrayLikes, combineUint8Arrays } from './utils.js'
 
 export class TurtleBranch {
+  /** @type {U8aTurtle} */
   #u8aTurtle
+  /** @type {Set.<ReadableStreamController>} */
+  #readableByteStreamControllers = new Set()
   /**
-   *
    * @param {string} name
    * @param {Recaller} recaller
    * @param {import('./U8aTurtle.js').U8aTurtle} u8aTurtle
@@ -29,6 +32,51 @@ export class TurtleBranch {
 
   append (uint8Array) {
     this.u8aTurtle = new U8aTurtle(uint8Array, this.u8aTurtle)
+    const controllers = this.#readableByteStreamControllers
+    controllers.forEach(controller => controller.enqueue(
+      combineUint8ArrayLikes([new Uint32Array([uint8Array.length]), uint8Array])
+    ))
+  }
+
+  /**
+   * @returns {ReadableStream}
+   */
+  makeReadableStream () {
+    const uint8Arrays = this.u8aTurtle?.exportUint8Arrays?.() ?? []
+    const controllers = this.#readableByteStreamControllers
+    let _controller
+    return new ReadableStream({
+      start (controller) {
+        _controller = controller
+        controllers.add(_controller)
+        uint8Arrays.forEach(uint8Array => _controller.enqueue(uint8Array))
+      },
+      cancel (reason) {
+        console.log('stream cancelled', { reason })
+        controllers.delete(_controller)
+      },
+      type: 'bytes'
+    })
+  }
+
+  /**
+   * @returns {WritableStream}
+   */
+  makeWritableStream () {
+    let inProgress = new Uint8Array()
+    let totalLength
+    const appender = this
+    return new WritableStream({
+      write (chunk) {
+        console.log(chunk)
+        inProgress = combineUint8Arrays([inProgress, chunk])
+        if (inProgress.length < 4) return
+        totalLength = new Uint32Array(inProgress.slice(0, 4).buffer)[0]
+        if (inProgress.length < totalLength + 4) return
+        appender.append(inProgress.slice(4, totalLength + 4))
+        inProgress = inProgress.slice(totalLength + 4)
+      }
+    })
   }
 
   get length () { return this.u8aTurtle?.length }
--- public/js/turtle/TurtleBranch.test.js
+++ public/js/turtle/TurtleBranch.test.js
@@ -2,7 +2,7 @@ import { globalRunner, urlToName } from '../../test/Runner.js'
 import { handleNextTick } from '../utils/nextTick.js'
 import { TurtleBranch } from './TurtleBranch.js'
 
-globalRunner.describe(urlToName(import.meta.url), suite => {
+globalRunner.only.describe(urlToName(import.meta.url), suite => {
   suite.it('passes through U8aTurtle changes', ({ assert }) => {
     const branch = new TurtleBranch('branch')
     const heights = []
@@ -23,5 +23,15 @@ globalRunner.describe(urlToName(import.meta.url), suite => {
     handleNextTick()
     assert.equal(heights, [undefined, 0, 1, 2, 1])
     assert.equal(branch.u8aTurtle.uint8Array, new Uint8Array([3, 4, 5, 6, 7, 8]))
+  })
+  suite.it('streams', async ({ assert }) => {
+    const primary = new TurtleBranch('primary')
+    const secondary = new TurtleBranch('secondary')
+    const readableStream = primary.makeReadableStream()
+    const writableStream = secondary.makeWritableStream()
+    readableStream.pipeTo(writableStream)
+    primary.append(new Uint8Array([1, 2, 3]))
+    await new Promise(resolve => setTimeout(resolve))
+    console.log(secondary.u8aTurtle.exportUint8Arrays())
   })
 })
