b26c2f546227b33bc76588c702fcafcf137a9bab
David Tudury <david.tudury@gmail.com>
2025-04-14 14:19:24 -0700

    wip

--- src/fsSync.js
+++ src/fsSync.js
@@ -10,15 +10,24 @@ export const ignored = /(?:\/node_modules\b|\/\..*|.*\.ico$|\.lock$|~$)/i
 const UPDATED_FILE = 'updated file'
 const REMOVED_FILE = 'removed file'
 const UPDATED_VALUE = 'updated value'
+const REMOVED_VALUE = 'removed value'
 
 /**
  * @param {Workspace} workspace
  * @param {string} [root=workspace.name]
- * @param {string} [jspath='']
+ * @param {string} [jspath='fs']
  */
 export function fsSync (workspace, root = workspace.name, jspath = 'fs') {
+  let greenLightPromise
+  const greenLight = async () => {
+    const previousPromise = greenLightPromise
+    let resolve
+    greenLightPromise = new Promise((...args) => { [resolve] = args })
+    await previousPromise
+    return resolve
+  }
   const nextActionByPath = {}
-  const jsobj = workspace.lastCommitValue?.[jspath] ?? {}
+  const jsobj = workspace.committedBranch.lookup('document', 'value', jspath, AS_REFS) ?? {}
   let commitDebounce
   const getPathHandlerFor = action => async path => {
     const relativePath = relative(root, path)
@@ -26,6 +35,7 @@ export function fsSync (workspace, root = workspace.name, jspath = 'fs') {
     const alreadyRunning = Object.hasOwn(nextActionByPath, relativePath)
     nextActionByPath[relativePath] = action
     if (alreadyRunning) return
+    const endGreenLight = await greenLight()
     while (nextActionByPath[relativePath]) {
       action = nextActionByPath[relativePath]
       nextActionByPath[relativePath] = null
@@ -36,20 +46,19 @@ export function fsSync (workspace, root = workspace.name, jspath = 'fs') {
         delete jsobj[relativePath]
       } else if (action === UPDATED_VALUE) {
         const newAddress = workspace.committedBranch.lookup('document', 'value', jspath, AS_REFS)?.[relativePath]
-        if (newAddress !== jsobj[relativePath]) {
-          if (!newAddress) {
-            await unlink(path)
-            delete jsobj[relativePath]
-          } else {
-            const file = workspace.committedBranch.lookup('document', 'value', jspath, relativePath)
-            if (!file) {
-              console.log(workspace.committedBranch.lookup('document', 'value', jspath))
-              throw new Error(`no file found at ${relativePath}`)
-            }
-            await writeFile(path, file, 'utf8')
-            jsobj[relativePath] = newAddress
-          }
+        if (!newAddress) {
+          console.error(workspace.committedBranch.lookup('document', 'value', jspath, AS_REFS))
+          throw new Error(`no address found at ${relativePath}`)
         }
+        jsobj[relativePath] = newAddress
+        const file = workspace.committedBranch.lookup('document', 'value', jspath, relativePath)
+        if (!file) {
+          console.error(workspace.committedBranch.lookup('document', 'value', jspath, AS_REFS))
+          throw new Error(`no file found at relativePath: ${relativePath}, address: ${newAddress}`)
+        }
+      } else if (action === REMOVED_VALUE) {
+        delete jsobj[relativePath]
+        await unlink(path)
       }
     }
     delete nextActionByPath[relativePath]
@@ -59,8 +68,9 @@ export function fsSync (workspace, root = workspace.name, jspath = 'fs') {
         const valueAsRefs = workspace.lookup('document', 'value', AS_REFS) || {}
         valueAsRefs[jspath] = workspace.upsert(jsobj, undefined, AS_REFS)
         const valueAddress = workspace.upsert(valueAsRefs, undefined, AS_REFS)
-        console.log('fs commit from local changes debounce')
+        console.log('fs commit from local changes debounce', jsobj)
         await workspace.commit(valueAddress, 'chokidar.watch', true)
+        endGreenLight()
       }, 500) // delay should take longer than the commit
     }
   }
@@ -68,24 +78,35 @@ export function fsSync (workspace, root = workspace.name, jspath = 'fs') {
     .on('add', getPathHandlerFor(UPDATED_FILE))
     .on('change', getPathHandlerFor(UPDATED_FILE))
     .on('unlink', getPathHandlerFor(REMOVED_FILE))
-  workspace.committedBranch.recaller.watch(`fsSync"${root}"`, () => {
-    const paths = workspace.committedBranch.lookup('document', 'value', jspath, AS_REFS)
-    if (!paths) return
-    let changed = false
+  workspace.committedBranch.recaller.watch(`fsSync"${root}"`, async () => {
+    workspace.committedBranch.lookup() // trigger recaller
+    const endGreenLight = await greenLight()
+    const newJsobj = workspace.committedBranch.lookup('document', 'value', jspath, AS_REFS)
+    if (!newJsobj) return endGreenLight()
+    const promises = []
     for (const path in jsobj) {
-      if (!paths[path]) {
-        changed = true
-        const handleRemovedValue = getPathHandlerFor(UPDATED_VALUE)
-        handleRemovedValue(join(root, path))
+      if (!newJsobj[path]) {
+        promises.push(unlink(join(root, path)))
       }
     }
-    for (const path in paths) {
-      if (paths[path] !== jsobj[path]) {
-        changed = true
-        const handleUpdatedValue = getPathHandlerFor(UPDATED_VALUE)
-        handleUpdatedValue(join(root, path))
+    for (const path in newJsobj) {
+      if (newJsobj[path] !== jsobj[path]) {
+        const relativePath = join(root, path)
+        const newAddress = workspace.committedBranch.lookup('document', 'value', jspath, AS_REFS)?.[relativePath]
+        if (!newAddress) {
+          console.error(workspace.committedBranch.lookup('document', 'value', jspath, AS_REFS))
+          throw new Error(`no address found at ${relativePath}`)
+        }
+        const file = workspace.committedBranch.lookup('document', 'value', jspath, relativePath)
+        if (!file) {
+          console.error(workspace.committedBranch.lookup('document', 'value', jspath, AS_REFS))
+          throw new Error(`no file found at relativePath: ${relativePath}, address: ${newAddress}`)
+        }
+        promises.push(writeFile(relativePath, file))
       }
     }
-    if (changed) console.log('fs update from workspace.committedBranch')
+    if (promises.length) console.log('fs update from workspace.committedBranch', workspace.lookup('document', 'value', jspath, AS_REFS), promises.length)
+    await Promise.all(promises)
+    endGreenLight()
   })
 }
