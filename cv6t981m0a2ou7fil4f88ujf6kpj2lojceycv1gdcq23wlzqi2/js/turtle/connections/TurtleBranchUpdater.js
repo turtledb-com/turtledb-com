a0517013b066b644b7b3383564d146dbc238d208
David Tudury <david.tudury@gmail.com>
2025-05-13 19:29:37 -0700

    bump

--- cv6t981m0a2ou7fil4f88ujf6kpj2lojceycv1gdcq23wlzqi2/js/turtle/connections/TurtleBranchUpdater.js
+++ cv6t981m0a2ou7fil4f88ujf6kpj2lojceycv1gdcq23wlzqi2/js/turtle/connections/TurtleBranchUpdater.js
@@ -31,7 +31,7 @@ export class TurtleBranchUpdater extends AbstractUpdater {
             incomingUint8ArrayAddresses.length = 0
           }
         }
-        console.log(`new TurtleBranchUpdater "${name}", #${allUpdaters.size}`)
+        console.log(`TurtleBranchUpdater(#${allUpdaters.size}): ${name}`)
         this.update(incomingUint8ArrayAddresses)
         lastU8aTurtle = this.turtleBranch.u8aTurtle
       }
