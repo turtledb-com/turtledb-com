ce376e47bc55c3934551e648b367e77cb534d962
David Tudury <david.tudury@gmail.com>
2025-08-13 03:48:06 -0700

    skip and only working

--- branches/.cv6t981m0a2ou7fil4f88ujf6kpj2lojceycv1gdcq23wlzqi2/js/utils/TestRunner.js
+++ branches/.cv6t981m0a2ou7fil4f88ujf6kpj2lojceycv1gdcq23wlzqi2/js/utils/TestRunner.js
@@ -2,11 +2,10 @@ import { TurtleDictionary } from '../turtle/TurtleDictionary.js'
 import { Assert } from './Assert.js'
 import { logError } from './logger.js'
 import { Recaller } from './Recaller.js'
-import { ERROR, FAIL, ONLY, PASS, RUNNER, RUNNING, SUITE, TEST, WAIT } from './TestRunnerConstants.js'
+import { ERROR, FAIL, ONLY, PASS, RUNNER, RUNNING, SKIP, SKIPPED, SUITE, TEST, WAIT } from './TestRunnerConstants.js'
 
 export function urlToName (url) {
   if (typeof window !== 'undefined' && window?.location?.origin && url.startsWith(window.location.origin)) {
-    console.log(new URL(window.location))
     url = url.slice(window.location.origin.length)
     const slashCpk = url.match(/\/[0-9A-Za-z]{41,51}(?=\/)/)?.[0]
     url = url.slice(slashCpk.length)
@@ -80,36 +79,7 @@ export class TestRunner {
         this.runState = FAIL
       }
     })()
-    await this.#runPromise
-  }
-
-  async run () {
-    this.#runPromise ??= (async () => {
-      this.runState = RUNNING
-      try {
-        if (this.#f) await this.#f(this)
-        await this.runChildren()
-      } catch (error) {
-        this.error = error
-        if (this.verbose) logError(() => console.error(error))
-        if (!(error instanceof TestRunnerError)) {
-          logError(() => console.error(error))
-          this.caught(`caught error: ${error.message}`, () => { throw new TestRunnerError(`${this.name}.run`, { cause: error }) })
-        }
-        this.runState = FAIL
-      }
-    })()
     return this.#runPromise
-  }
-
-  async rerunChildren () {
-    this.runState = RUNNING
-    try {
-      await this.runChildren()
-    } catch (error) {
-      this.error = error
-      this.runState = FAIL
-    }
   }
 
   async runChildren () {
@@ -117,9 +87,10 @@ export class TestRunner {
       ++this.runIndex
       const errors = []
       const hasOnly = this.#children.some(child => child.type === ONLY)
-      console.log({hasOnly, name: this.name})
       for (const child of this.#children) {
-        if (!hasOnly || child.type === ONLY) await child.run()
+        if (hasOnly && child.type !== ONLY) child.runState = SKIPPED
+        if (child.type === SKIP) child.runState = SKIPPED
+        if (child.runState !== SKIPPED) await child.run()
         if (child.runState === FAIL) errors.push(child.error)
       }
       if (errors.length) {
@@ -155,28 +126,17 @@ export class TestRunner {
     this.#runState = runState
   }
 
-  get only () {
-    const child = new TestRunner('only', ONLY, this, () => undefined, this.recaller, this.verbose)
-    this.#children.push(child)
-    this.#runChildrenPromise = undefined
-    // this.recaller.reportKeyMutation(this, 'children', 'appendChild', this.name)
-    return child
-  }
-
   /**
    * @param {string} name
    * @param {(runner: TestRunner) => any} f
    * @param {string} type
    * @returns {TestRunner}
    */
-  async appendChild (name, f, type) {
+  appendChild (name, f, type) {
     const child = new TestRunner(name, type, this, f, this.recaller, this.verbose)
     this.#children.push(child)
     this.#runChildrenPromise = undefined
-    // this.recaller.reportKeyMutation(this, 'children', 'appendChild', this.name)
-    if (this.runState === RUNNING) {
-      await child.run()
-    }
+    this.recaller.reportKeyMutation(this, 'children', 'appendChild', this.name)
     return child
   }
 
@@ -191,7 +151,7 @@ export class TestRunner {
    * @param {(suite: TestRunner) => any} f
    * @returns {TestRunner}
    */
-  async describe (name, f) {
+  describe (name, f) {
     return this.appendChild(name, f, SUITE)
   }
 
@@ -200,7 +160,7 @@ export class TestRunner {
    * @param {(test: TestRunner) => any} f
    * @returns {TestRunner}
    */
-  async it (name, f) {
+  it (name, f) {
     return this.appendChild(name, f, TEST)
   }
 
@@ -209,7 +169,7 @@ export class TestRunner {
    * @param {(test: TestRunner) => any} f
    * @returns {TestRunner}
    */
-  async test (name, f) {
+  test (name, f) {
     return this.appendChild(name, f, TEST)
   }
 
@@ -218,8 +178,22 @@ export class TestRunner {
    * @param {(test: TestRunner) => any} f
    * @returns {TestRunner}
    */
-  async caught (name, f) {
+  caught (name, f) {
     return this.appendChild(name, f, ERROR)
+  }
+
+  /**
+   * @returns {TestRunner}
+   */
+  get only () {
+    return this.appendChild('only', () => {}, ONLY)
+  }
+
+  /**
+   * @returns {TestRunner}
+   */
+  get skip () {
+    return this.appendChild('skip', () => {}, SKIP)
   }
 }
 
